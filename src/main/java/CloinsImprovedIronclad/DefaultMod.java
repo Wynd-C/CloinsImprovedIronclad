eda2ef4c0c641ee873743e2cc4f28546a8623902
Wynd <707royale@gmail.com>
2021-01-30 21:27:14 -0300

    Changed image paths relating to getModID() 2, electric boogaloo.

--- src/main/java/CloinsImprovedIronclad/DefaultMod.java
+++ src/main/java/CloinsImprovedIronclad/DefaultMod.java
@@ -138,27 +138,27 @@ public class DefaultMod implements
     // =============== MAKE IMAGE PATHS =================
     
     public static String makeCardPath(String resourcePath) {
-        return getModID() + "images/cards/" + resourcePath;
+        return getModID() + "Resources/images/cards/" + resourcePath;
     }
     
     public static String makeRelicPath(String resourcePath) {
-        return getModID() + "images/relics/" + resourcePath;
+        return getModID() + "Resources/images/relics/" + resourcePath;
     }
     
     public static String makeRelicOutlinePath(String resourcePath) {
-        return getModID() + "images/relics/outline/" + resourcePath;
+        return getModID() + "Resources/images/relics/outline/" + resourcePath;
     }
     
     public static String makeOrbPath(String resourcePath) {
-        return getModID() + "images/orbs/" + resourcePath;
+        return getModID() + "Resources/images/orbs/" + resourcePath;
     }
     
     public static String makePowerPath(String resourcePath) {
-        return getModID() + "images/powers/" + resourcePath;
+        return getModID() + "Resources/images/powers/" + resourcePath;
     }
     
     public static String makeEventPath(String resourcePath) {
-        return getModID() + "images/events/" + resourcePath;
+        return getModID() + "Resources/images/events/" + resourcePath;
     }
     
     // =============== /MAKE IMAGE PATHS/ =================
