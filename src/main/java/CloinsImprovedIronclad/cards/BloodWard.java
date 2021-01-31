28d5aa89187d1814edad401e2fc806e436705442
Wynd <707royale@gmail.com>
2021-01-31 01:37:48 -0300

    First working prototype including test card

---
+++ src/main/java/CloinsImprovedIronclad/cards/BloodWard.java
@@ -0,0 +1,72 @@
+package CloinsImprovedIronclad.cards;
+
+import basemod.AutoAdd;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.LoseHPAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import CloinsImprovedIronclad.DefaultMod;
+import CloinsImprovedIronclad.characters.TheDefault;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+
+import static CloinsImprovedIronclad.DefaultMod.makeCardPath;
+
+
+public class BloodWard extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(BloodWard.class.getSimpleName()); // USE THIS ONE FOR THE TEMPLATE;
+    public static final String IMG = makeCardPath("Skill.png");// "public static final String IMG = makeCardPath("BloodWard.png");
+    // This does mean that you will need to have an image with the same NAME as the card in your image folder for it to run correctly.
+
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON; //  Up to you, I like auto-complete on these
+    private static final CardTarget TARGET = CardTarget.SELF;  //   since they don't change much.
+    private static final CardType TYPE = CardType.SKILL;       //
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;  // COST = 1
+    private static final int UPGRADED_COST = 1; // UPGRADED_COST = 1
+
+    private static final int BLOCK = 13;    // DAMAGE = 13
+    private static final int UPGRADE_PLUS_BLOCK = 2;  // UPGRADE_PLUS_DMG = 15
+
+    private static final int MAGICNUMBER = 2;
+
+    // /STAT DECLARATION/
+
+
+    public BloodWard() { // public BloodWard() - This one and the one right under the imports are the most important ones, don't forget them
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseBlock = BLOCK;
+        baseMagicNumber = MAGICNUMBER;
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new LoseHPAction(p, p, baseMagicNumber));
+        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, block));
+    }
+
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_PLUS_BLOCK);
+            upgradeBaseCost(UPGRADED_COST);
+            initializeDescription();
+        }
+    }
+}
