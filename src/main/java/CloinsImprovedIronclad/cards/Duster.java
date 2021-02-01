c942dd31546399f3f0e01f206960a768297d8cd3
Wynd <707royale@gmail.com>
2021-02-01 19:44:17 -0300

    added dusterw

---
+++ src/main/java/CloinsImprovedIronclad/cards/Duster.java
@@ -0,0 +1,66 @@
+package CloinsImprovedIronclad.cards;
+
+import basemod.AutoAdd;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.utility.ConditionalDrawAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import CloinsImprovedIronclad.DefaultMod;
+import CloinsImprovedIronclad.characters.TheDefault;
+
+import static CloinsImprovedIronclad.DefaultMod.makeCardPath;
+
+// public class Duster extends AbstractDynamicCard
+public class Duster extends AbstractDynamicCard {
+
+
+
+    public static final String ID = DefaultMod.makeID(Duster.class.getSimpleName()); // USE THIS ONE FOR THE TEMPLATE;
+    public static final String IMG = makeCardPath("Skill.png");// "public static final String IMG = makeCardPath("Duster.png");
+    // This does mean that you will need to have an image with the same NAME as the card in your image folder for it to run correctly.
+
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON; //  Up to you, I like auto-complete on these
+    private static final CardTarget TARGET = CardTarget.SELF;  //   since they don't change much.
+    private static final CardType TYPE = CardType.SKILL;       //
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;  // COST =
+
+    private static final int MAGICNUMBER = 3;
+    private static final int UPGRADE_PLUS_MAGICNUMBER = 1;
+
+    // /STAT DECLARATION/
+
+
+    public Duster() { // public Duster() - This one and the one right under the imports are the most important ones, don't forget them
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = this.MAGICNUMBER;
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m){
+        this.addToBot(new ConditionalDrawAction(this.baseMagicNumber, CardType.STATUS));
+    }
+
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_MAGICNUMBER);
+            initializeDescription();
+        }
+    }
+}
