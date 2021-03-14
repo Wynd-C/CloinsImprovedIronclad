7b3aff6539c6bd6b86e77b51303e4916c0f7504b
Wynd <707royale@gmail.com>
2021-03-13 22:10:19 -0300

    Added Pinch

--- src/main/java/CloinsImprovedIronclad/cards/Pinch.java
+++ src/main/java/CloinsImprovedIronclad/cards/Pinch.java
@@ -2,6 +2,7 @@ package CloinsImprovedIronclad.cards;
 
 import basemod.AutoAdd;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -9,35 +10,17 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import CloinsImprovedIronclad.DefaultMod;
 import CloinsImprovedIronclad.characters.TheDefault;
+import com.megacrit.cardcrawl.powers.StrengthPower;
 
 import static CloinsImprovedIronclad.DefaultMod.makeCardPath;
 
 // public class ${NAME} extends AbstractDynamicCard
-@AutoAdd.Ignore // Remove this line when you make a template. Refer to https://github.com/daviscook477/BaseMod/wiki/AutoAdd if you want to know what it does.
 public class Pinch extends AbstractDynamicCard {
 
-    /*
-     * "Hey, I wanna make a bunch of cards now." - You, probably.
-     * ok cool my dude no problem here's the most convenient way to do it:
-     *
-     * Copy all of the code here (Ctrl+A > Ctrl+C)
-     * Ctrl+Shift+A and search up "file and code template"
-     * Press the + button at the top and name your template whatever it is for - "AttackCard" or "PowerCard" or something up to you.
-     * Read up on the instructions at the bottom. Basically replace anywhere you'd put your cards name with ${NAME}
-     * And then you can do custom ones like ${DAMAGE} and ${TARGET} if you want.
-     * I'll leave some comments on things you might consider replacing with what.
-     *
-     * Of course, delete all the comments and add anything you want (For example, if you're making a skill card template you'll
-     * likely want to replace that new DamageAction with a gain Block one, and add baseBlock instead, or maybe you want a
-     * universal template where you delete everything unnecessary - up to you)
-     *
-     * You can create templates for anything you ever want to. Cards, relics, powers, orbs, etc. etc. etc.
-     */
-
-    // TEXT DECLARATION
+     // TEXT DECLARATION
 
     // public static final String ID = DefaultMod.makeID(${NAME}.class.getSimpleName()); // USE THIS ONE FOR THE TEMPLATE;
-    public static final String ID = DefaultMod.makeID("DefaultCommonAttack"); // DELETE THIS ONE.
+    public static final String ID = DefaultMod.makeID("Pinch"); // DELETE THIS ONE.
     public static final String IMG = makeCardPath("Attack.png");// "public static final String IMG = makeCardPath("${NAME}.png");
     // This does mean that you will need to have an image with the same NAME as the card in your image folder for it to run correctly.
 
@@ -52,11 +35,13 @@ public class Pinch extends AbstractDynamicCard {
     private static final CardType TYPE = CardType.ATTACK;       //
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
 
-    private static final int COST = 1;  // COST = ${COST}
-    private static final int UPGRADED_COST = 0; // UPGRADED_COST = ${UPGRADED_COST}
+    private static final int COST = 0;  // COST = ${COST}
 
-    private static final int DAMAGE = 7;    // DAMAGE = ${DAMAGE}
-    private static final int UPGRADE_PLUS_DMG = 2;  // UPGRADE_PLUS_DMG = ${UPGRADED_DAMAGE_INCREASE}
+    private static final int DAMAGE = 2;    // DAMAGE = ${DAMAGE}
+    private static final int UPGRADE_PLUS_DMG = -1;  // UPGRADE_PLUS_DMG = ${UPGRADED_DAMAGE_INCREASE}
+    private static final int ATTACKS = 1;
+    private static final int UPGRADE_PLUS_ATTACKS = 1;
+    private static final int STRENGTH = 1;
 
     // /STAT DECLARATION/
 
@@ -64,24 +49,28 @@ public class Pinch extends AbstractDynamicCard {
     public Pinch() { // public ${NAME}() - This one and the one right under the imports are the most important ones, don't forget them
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = DAMAGE;
+        magicNumber = baseMagicNumber = ATTACKS;
+        defaultSecondMagicNumber = defaultBaseSecondMagicNumber = STRENGTH;
     }
 
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        for(int i = 0; i < magicNumber; i++)
         AbstractDungeon.actionManager.addToBottom(
-                new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
-    }
+                new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new StrengthPower(AbstractDungeon.player, defaultBaseSecondMagicNumber)));
+        }
 
 
     // Upgraded stats.
     @Override
     public void upgrade() {
-        if (!upgraded) {
-            upgradeName();
-            upgradeDamage(UPGRADE_PLUS_DMG);
-            upgradeBaseCost(UPGRADED_COST);
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeDamage(UPGRADE_PLUS_DMG);
+            this.upgradeDefaultSecondMagicNumber(UPGRADE_PLUS_ATTACKS);
             initializeDescription();
         }
     }
