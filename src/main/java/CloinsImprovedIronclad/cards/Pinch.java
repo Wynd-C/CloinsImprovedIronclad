598365c04791d75dfd1689ae97bb5b24e4d9379e
Wynd <707royale@gmail.com>
2021-03-13 22:15:21 -0300

    Fixed Card Name Exception

--- src/main/java/CloinsImprovedIronclad/cards/Pinch.java
+++ src/main/java/CloinsImprovedIronclad/cards/Pinch.java
@@ -19,8 +19,7 @@ public class Pinch extends AbstractDynamicCard {
 
      // TEXT DECLARATION
 
-    // public static final String ID = DefaultMod.makeID(${NAME}.class.getSimpleName()); // USE THIS ONE FOR THE TEMPLATE;
-    public static final String ID = DefaultMod.makeID("Pinch"); // DELETE THIS ONE.
+    public static final String ID = DefaultMod.makeID(Pinch.class.getSimpleName()); // USE THIS ONE FOR THE TEMPLATE;
     public static final String IMG = makeCardPath("Attack.png");// "public static final String IMG = makeCardPath("${NAME}.png");
     // This does mean that you will need to have an image with the same NAME as the card in your image folder for it to run correctly.
 
