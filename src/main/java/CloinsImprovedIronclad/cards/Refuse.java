181339b14d040be50ffb10c1cb9e1ff64ed70352
Wynd <707royale@gmail.com>
2021-04-02 22:04:36 -0300

    Added Refuse

---
+++ src/main/java/CloinsImprovedIronclad/cards/Refuse.java
@@ -0,0 +1,87 @@
+package CloinsImprovedIronclad.cards;
+
+import basemod.AutoAdd;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import CloinsImprovedIronclad.DefaultMod;
+import CloinsImprovedIronclad.characters.TheDefault;
+import com.badlogic.gdx.math.MathUtils;
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.DamageRandomEnemyAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.input.InputHelper;
+import com.megacrit.cardcrawl.relics.AbstractRelic.LandingSound;
+import com.megacrit.cardcrawl.relics.AbstractRelic.RelicTier;
+
+
+import static CloinsImprovedIronclad.DefaultMod.makeCardPath;
+
+
+public class Refuse extends AbstractDynamicCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = DefaultMod.makeID(Refuse.class.getSimpleName()); // USE THIS ONE FOR THE TEMPLATE;
+    public static final String IMG = makeCardPath("Skill.png");// "public static final String IMG = makeCardPath("${NAME}.png");
+
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON; //  Up to you, I like auto-complete on these
+    private static final CardTarget TARGET = CardTarget.NONE;  //   since they don't change much.
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = CardColor.GREEN;
+
+    private static final int COST = -2;  // COST = ${COST}
+
+    private static final int DAMAGE = 5;    // DAMAGE = ${DAMAGE}
+    private static final int UPGRADE_PLUS_DMG = 3;  // UPGRADE_PLUS_DMG = ${UPGRADED_DAMAGE_INCREASE}
+
+    // /STAT DECLARATION/
+
+
+    public Refuse() { // public ${NAME}() - This one and the one right under the imports are the most important ones, don't forget them
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+        this.isMultiDamage = true;
+    }
+
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+    }
+
+    public boolean canUse(AbstractPlayer p, AbstractMonster m){
+        return false;
+    }
+
+    public void triggerOnManualDiscard() {
+        AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction(AbstractDungeon.player, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.NONE));
+    }
+
+    public AbstractCard makeCopy() {return new Refuse(); }
+
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            initializeDescription();
+        }
+    }
+}
